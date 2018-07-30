bd76387de588028bc2e1b6e229e811e13d8238b9
Mike G <followthroughworks@gmail.com>
2018-07-30 09:46:41 -0400

    Add django-extensions for shell_plus

--- lifesheet/settings.py
+++ lifesheet/settings.py
@@ -51,7 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
