3ed8195048f7f1a4a92bcd723e718a5682ceee02
Mike G <followthroughworks@gmail.com>
2018-07-30 13:14:38 -0400

    Unbroke urls I hope

--- lifesheet/urls.py
+++ lifesheet/urls.py
@@ -19,7 +19,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$/', include('main_app.urls')),
+    url(r'^$', include('main_app.urls')),
     #url('^accounts/', include('django.contrib.auth.urls')),
     url(r'^api/', include('main_app.drf_urls')),
 ]
