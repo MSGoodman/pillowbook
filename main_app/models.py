6d4e7f99c990340bd4f72ba272a05e997fabdd4e
Mike <followthroughworks@gmail.com>
2018-08-13 14:26:29 -0400

    Added season and episode nums to television, addresses #5

--- main_app/models.py
+++ main_app/models.py
@@ -217,6 +217,8 @@ class Television(BaseModel):
     date = models.DateField(default=datetime.now)
     series = models.TextField()
     episode = models.TextField()
+    season_num = models.CharField(max_length=2, blank = True, null = True)
+    episode_num = models.CharField(max_length=2, blank = True, null = True)
     rating = models.DecimalField(validators=[MinValueValidator(0), MaxValueValidator(5)], default = 3, max_digits=2, decimal_places=1)
     review = models.TextField(blank = True, null = True)
 
